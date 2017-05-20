eddb6d8c05474e0875ddf5d490f458725efbb1b7
Karl Broman <kbroman@gmail.com>
2017-05-19 22:48:05 -0400

    Add mc_connect, mc_send, mc_receive

---
+++ R/connect.R
@@ -0,0 +1,33 @@
+#' Create connection to minecraft server
+#'
+#' Create connection to minecraft server
+#'
+#' @param address The url for the minecraft server
+#' @param port The port to use for the connection
+#'
+#' @return A socket connection to the minecraft server, hopefully.
+#'
+#' @export
+
+mc_connect <- function(address="localhost", port=4711)
+{
+    socketConnection(host=address, port, open="r+b", encoding="CP437")
+}
+
+
+mc_send <- function(text, con)
+{
+    writeLines(text, con=con, useBytes=TRUE)
+}
+
+
+mc_receive <- function(con)
+{
+    readLines(con, encoding="CP437")
+}
+
+mc_sendreceive <- function(text, con)
+{
+    mc_send(text, con)
+    mc_receive(con)
+}
