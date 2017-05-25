21f43d1464e08ffef77360e2a1375b3d39759fba
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-25 12:04:56 -0600

    Add function to set player position

---
+++ R/setPlayerPos.R
@@ -0,0 +1,29 @@
+#' Change player position
+#'
+#' Move player to position (x,y,z) 
+#'
+#' @param con Socket connection to minecraft server
+#' @param x north/south position
+#' @param y height
+#' @param z east/west position
+#'
+#' @return None.
+#'
+#' @examples
+#' \dontrun{
+#' mc <- mc_connect()
+#' p <- getPlayerPost(mc)
+#' setPlayerPos(mc, 0, p + 5, 0)
+#' }
+#'
+#' @export
+#' 
+setPlayerPos <- function(con, x,y,z)
+{
+  x <- round(x)
+  y <- round(y)
+  z <- round(z)
+  
+  mc_send(merge_data("player.setPos", x, y, z), con)
+  
+}
