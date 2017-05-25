6d4f1817ba659e1943caad687d38be1120ab89d3
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-25 15:48:59 -0600

    Add entity to `getPlayerRotation` also, update documentation and change function names for `getPlayerPitch` and `getPlayerDirection`

--- R/getPlayerOrientation.R
+++ R/getPlayerOrientation.R
@@ -1,27 +1,31 @@
-# player.getRotation()
-# player.getPitch()
-# player.getDirection()  [vector of three values separated by commas]
-
-
 #' Get player rotation
 #'
-#' Get player rotation from current position
+#' Get the current rotation of the player.
 #'
-#' @return double of length 1 with rotatio
+#' @return A double vector of length 1 with a value between 
+#' -360 and 360 giving the angle of rotation of the player.  
 #'
 #'
 #' @examples
 #' \dontrun{
 #' getPlayerRotation()
+#' 
+#' example_playerId <- getPlayerIds()[1]
+#' getPlayerRotation(example_playerId)
 #' }
 #'
-#' @seealso [getPlayerDirection]
+#' @seealso \code{\link{getPlayerDirection}}
 #'
 #' @export
 
-getPlayerRotation <- function()
+getPlayerRotation <- function(player_id = NULL)
 {
-  p <- mc_sendreceive("player.getRotation()")
+  if(is.null(id)){
+    p <- mc_sendreceive("player.getRotation()")    
+  } else {
+    p <- mc_sendreceive(merge_data("entity.getRotation", player_id))
+  }
+
   as.numeric(p)
 }
 
@@ -37,7 +41,7 @@ getPlayerRotation <- function()
 #' \dontrun{
 #' getPitch()
 #' }
-getPitch <- function() {
+getPlayerPitch <- function() {
   
   p <- mc_sendreceive("player.getPitch()")
   as.numeric(p)
@@ -47,16 +51,16 @@ getPitch <- function() {
 
 #' Get direction vector
 #'
-#' Get direction vector
+#' Get a vector describing the current direction a player is facing. 
 #'
-#' @return A vector of length 3 with coordinates of current direction 
+#' @return A numeric vector of length 3 with coordinates of current direction 
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' getDirection()
 #' }
-getDirection <- function() {
+getPlayerDirection <- function() {
   
   z <- mc_sendreceive("player.getDirection()")
   as.numeric(strsplit(z, ",")[[1]])
