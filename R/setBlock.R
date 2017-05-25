19ea792a4e2805a66bf188a725c3aeffe5443a34
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-25 16:21:15 -0600

    Change parameter in `setBlocks` to `id` instead of `block_id`
    Again, to be consistent in parameter names across the package. Also, expand documentation.

--- R/setBlock.R
+++ R/setBlock.R
@@ -33,16 +33,11 @@ setBlock <- function(x, y, z, id, style=0)
 
 #' Place blocks in a cuboid
 #'
-#' Place blocks in cuboid defined by (x0,y0,z0) and (x1,y1,z1)
+#' Place blocks of a single type (specified by \code{id}) in the cuboid
+#' with opposite corners at the positions (x0, y0, z0) and (x1, y1, z1).
 #'
-#' @param x0 north/south position
-#' @param y0 height
-#' @param z0 east/west position
-#' @param x1 north/south position
-#' @param y1 height
-#' @param z1 east/west position
-#'
-#' @param block_id block id
+#' @inheritParams getBlocks
+#' @inheritParams find_item
 #'
 #' @return None.
 #'
@@ -50,12 +45,12 @@ setBlock <- function(x, y, z, id, style=0)
 #' \dontrun{
 #' mc_connect()
 #' h <- getHeight(0,0)
-#' setBlock(0,h,0,  46)
+#' setBlocks(0, h, 0, 1, h + 3, 2, id = 46)
 #' }
 #'
 #' @export
 
-setBlocks <- function(x0,y0,z0, x1,y1,z1,  block_id)
+setBlocks <- function(x0,y0,z0, x1,y1,z1,  id)
 {
     x0 <- round(x0)
     y0 <- round(y0)
@@ -63,8 +58,8 @@ setBlocks <- function(x0,y0,z0, x1,y1,z1,  block_id)
     x1 <- round(x1)
     y1 <- round(y1)
     z1 <- round(z1)
-    block_id <- round(block_id)
+    id <- round(id)
 
-    mc_send(merge_data("world.setBlocks", x0, y0, z0, x1, y1, z1, block_id))
+    mc_send(merge_data("world.setBlocks", x0, y0, z0, x1, y1, z1, id))
 
 }
