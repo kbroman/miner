65284b1dc2d7594321108499d069a74b3769196f
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-25 16:10:29 -0600

    Combine `getPlayerPos` and `getPlayerTilePos`. Also, flesh out documentation.

--- R/getPlayerPos.R
+++ R/getPlayerPos.R
@@ -1,62 +1,40 @@
-#' Get player tile position
-#'
-#' Get player tile position
-#'
-#' @param player_id Optional player ID
-#'
-#' @return vector (x,y,z)
-#'
-#' @examples
-#' \dontrun{
-#' mc_connect()
-#' getPlayerTilePos()
-#'
-#' example_playerId <- getPlayerIds()[1]
-#' getPlayerPos(example_playerId)
-#' }
-#'
-#' @seealso [getPlayerPos]
-#'
-#' @export
-
-getPlayerTilePos <- function(player_id = NULL)
-{
-  if(is.null(player_id)){
-    z <- mc_sendreceive("player.getTile()")
-  } else {
-    z <- mc_sendreceive(merge_data("entity.getTile", player_id))
-  }
-
-    as.numeric( strsplit(z, ",")[[1]] )
-}
-
-
 #' Get player position
 #'
 #' Get entity position. The default is to get the player position, but other
 #' positions can be gotten using the \code{id} argument.
 #'
-#' @param player_id  Entity id
-#' @return vector (x,y,z)
+#' @param player_id  Integer giving the ID of a player. You can find IDs of all 
+#'   current players using \code{\link{getPlayerIds}}.
+#' @param tile Logical value specifying whether to truncate the output position to 
+#'   an integer (i.e., the location of the tile on which the player is 
+#'   positioned).
+#'   
+#' @return A numeric vector of length three giving the position (x, y, and z) of the 
+#'   requested player. 
 #'
 #' @examples
 #' \dontrun{
 #' mc_connect()
 #' getPlayerPos()
+#' getPlayerPos(tile = TRUE)
 #'
 #' example_entity <- getPlayerIds()[1]
 #' getPlayerPos(example_entity)
 #' }
 #'
-#' @seealso [getPlayerPos]
-#'
 #' @export
-getPlayerPos <- function(player_id = NULL)
+getPlayerPos <- function(player_id = NULL, tile = FALSE)
 {
   if(is.null(player_id)){
     z <- mc_sendreceive("player.getPos()")
   } else {
     z <- mc_sendreceive(merge_data("entity.getPos", player_id))
   }
-    as.numeric( strsplit(z, ",")[[1]] )
+    out <- as.numeric( strsplit(z, ",")[[1]] )
+    
+    if(tile == TRUE){
+      out <- floor(out)
+    }
+    
+    out
 }
