7d4c6089c0ecf92c543a0d66f013113c07072184
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-25 16:46:11 -0600

    Move `getChatPosts` to chat function file. Also...
    rename the function (take out "Events") and update documentation

--- R/chat.R
+++ R/chat.R
@@ -19,3 +19,25 @@
 chatPost <- function(text) {
     mc_send(sprintf('chat.post(%s)', text))
 }
+
+#' Pull the most recent chat message
+#' 
+#' Returns the chat posts that have been posted recently and since the
+#' last time the function was run. It is possible that this does not 
+#' return chat posts that were posted using \code{\link{chatPost}}.
+#' 
+#' @return A dataframe of recent chat posts with columns for the player 
+#'    id of the poster and the message posted
+#' 
+#' @seealso \code{\link{getEventsBlockHits}}
+#' 
+#' @examples \dontrun{
+#' mc_connect()
+#' getEventsChatPosts()
+#' }
+#' 
+#' @export
+getChatPosts <- function() {
+  mcsplit(mc_sendreceive('events.chat.posts()'),
+          c('player', 'message'))
+}
