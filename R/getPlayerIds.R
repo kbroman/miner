0f1255e36feefb926b6d062ef64c1696886f64d0
David Smith <davidsmi@microsoft.com>
2017-05-25 14:42:29 -0700

    Merge conflict resolved

--- R/getPlayerIds.R
+++ R/getPlayerIds.R
@@ -2,13 +2,8 @@
 #'
 #' Get the IDs of all player currently in the world. 
 #'
-<<<<<<< HEAD
-#' @return set of integers with one unique id per player.
-#' player ids are listed in the order they joined the game world.
-=======
-#' @return A numeric vector with the IDs of each player currently 
-#'    in the Minecraft world. 
->>>>>>> fb168144fc99b2b5cfae2792310333e53b847799
+#' @return A numeric vector with the IDs of each player currently
+#'   in the MineCraft world. Player ids are listed in the order they joined the game world.
 #'
 #' @examples
 #' \dontrun{
