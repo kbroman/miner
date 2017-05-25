e138bfe2fa1ac1160156ca861b8054f556ebc233
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-25 12:44:19 -0600

    Expand `getPlayerPos` so it can get other entity positions

--- R/getPlayerPos.R
+++ R/getPlayerPos.R
@@ -25,9 +25,11 @@ getPlayerTilePos <- function(con)
 
 #' Get player position
 #'
-#' Get player position
+#' Get entity position. The default is to get the player position, but other
+#' positions can be gotten using the \code{id} argument.
 #'
 #' @param con Socket connection to minecraft server
+#' @param id  Entity id
 #'
 #' @return vector (x,y,z)
 #'
@@ -35,14 +37,19 @@ getPlayerTilePos <- function(con)
 #' \dontrun{
 #' mc <- mc_connect()
 #' getPlayerPos(mc)
+#' # getPlayerPos(mc, id = [what would this look like?])
 #' }
 #'
 #' @seealso [getPlayerPos]
 #'
 #' @export
 
-getPlayerPos <- function(con)
+getPlayerPos <- function(con, id = NULL)
 {
-    z <- mc_sendreceive("player.getPos()", con)
+  if(is.null(id)){
+    z <- mc_sendreceive("player.getPos()", con)    
+  } else {
+    z <- mc_sendreceive(merge_data("entity.getPos", id), con)
+  }
     as.numeric( strsplit(z, ",")[[1]] )
 }
