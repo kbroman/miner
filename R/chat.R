f74c37a10f09c8f45b5cd8285dbaf2718ecb8a29
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-25 14:47:45 -0600

    Flesh out `chatPost` helpfile

--- R/chat.R
+++ R/chat.R
@@ -1,14 +1,21 @@
 #' Post to chat
-#' @param text string
-#' @export
+#' 
+#' Post a message to Minecraft chat. 
+#' 
+#' @param text A character string with the message you would like
+#'    to post. 
+#' 
+
 #' @examples \dontrun{
 #' chatPost('foobar')
-#'
+#' 
 #' ## Post the first few digits of pi to chat
 #' for (p in strsplit(as.character(round(pi, 4)), '')[[1]]) {
 #'     chatPost(p)
+#'   }
 #' }
-#' }
+#' 
+#' @export
 chatPost <- function(text) {
     mc_send(glue(sprintf('chat.post(%s)', shQuote(text))))
 }
