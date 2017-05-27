71e50a7aa65de6000cb4b7342115e031bd3c14ae
Karl Broman <kbroman@gmail.com>
2017-05-26 23:10:21 -0700

    extra_vignettes/minerplot.R: fix render_bitmap()
     - now implement all possible orientations for render_bitmap 
       (there are 24 of them, though for text, 4 are upside down
       and so maybe not of interest)

--- extra_vignettes/minerplot.R
+++ extra_vignettes/minerplot.R
@@ -103,41 +103,75 @@ round_char <- function (x, digits = 1)
 # render a bitmap in minecraft
 #
 # bitmap is matrix of 0's and 1's
-# topleft is a vector of length 3 giving (x=north/south, y=up/down, z=east/west)
+# bottomleft is a vector of length 3 giving position of lower left corner of first letter
+#      (x=north/south, y=up/down, z=east/west)
 # id, style are single integers for block type
-# fixed_dim indicates which dimension is fixed
+# dir = direction that the word runs
+# top = direction that the top of the word points
+#   Note that not all (dir,top) pairs are valid
+#   For example, if dir=north or south, top can't be north or south
 render_bitmap <- function(bitmap, bottomleft, id, style=0,
-                          fixed_dim = c("z", "y", "x"),
-                          reverse=FALSE)
+                          dir=c("north", "south", "east", "west", "up", "down"),
+                          top=c("east", "west", "up", "down", "north", "south"))
 {
-    fixed_dim <- match.arg(fixed_dim)
+    require(miner)
+
+    dir <- match.arg(dir)
+    top <- match.arg(top)
     x0 <- bottomleft[1]
     y0 <- bottomleft[2]
     z0 <- bottomleft[3]
 
-    require(miner)
-
-    if(reverse) {
-        bitmap <- bitmap[,ncol(bitmap):1]
-        if(fixeddim=="z") bottomleft <- bottomleft + c(ncol(bitmap), 0, 0)
-        if(fixeddim=="x") bottomleft <- bottomleft + c(0, 0, ncol(bitmap))
-        if(fixeddim=="y") bottomleft <- bottomleft + c(0, 0, ncol(bitmap))
-
-        dir <- -1
-    }
-    else {
-        dir <- +1
+    # check that (dir,top) pair is valid
+    dir_pairs <- list(c("north", "south"),
+                      c("east", "west"),
+                      c("up", "down"))
+    for(i in seq_along(dir_pairs)) {
+        if(dir %in% dir_pairs[[i]] && top %in% dir_pairs[[i]])
+            stop("Can't have dir==\"", dir, '" and top=="', top, '"')
     }
 
-    for(i in 1:nrow(bitmap)) {
-        for(j in 1:ncol(bitmap)) {
+    # east =  positive x   west =  negative x
+    # up =    positive y   down =  negative y
+    # south = positive z   north = negative z
+
+    # multiplier about direction we're going
+    if(dir %in% c("east", "south", "up")) fmult <- 1
+    else fmult <- -1
+    if(top %in% c("east", "south", "up")) tmult <- -1
+    else tmult <- +1
+
+    ns <- c("north", "south")
+    ew <- c("east", "west")
+    ud <- c("up", "down")
+
+    x <- x0
+    y <- y0
+    z <- z0
+    for(i in 1:nrow(bitmap)) { # top -> bottom
+        for(j in 1:ncol(bitmap)) { # left -> right
             if(bitmap[i,j]) {
-                if(fixed_dim=="z")
-                    setBlock(x0+j,y0+nrow(bitmap)-i,z0,id,style)
-                else if(fixed_dim=="y")
-                    setBlock(x0+j,y0,z0+i,id,style)
-                else # x
-                    setBlock(x0,y0+nrow(bitmap)-i,z0+j,id,style)
+                if(dir %in% ns) {
+                    z <- z0 + fmult*(j-1)
+                }
+                else if (dir %in% ew) {
+                    x <- x0 + fmult*(j-1)
+                }
+                else { # up/down
+                    y <- y0 + fmult*(j-1)
+                }
+
+                if(top %in% ns) {
+                    z <- z0 + tmult*(i-1 - nrow(bitmap))
+                }
+                else if(top %in% ew) {
+                    x <- x0 + tmult*(i-1 - nrow(bitmap))
+                }
+                else { # up/down
+                    y <- y0 + tmult*(i-1 - nrow(bitmap))
+                }
+
+                setBlock(x, y, z, id, style)
             }
         }
     }
