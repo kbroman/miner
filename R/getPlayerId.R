be2398bd18e0145ad9001546350ea864d0cfa3a8
Karl Broman <kbroman@gmail.com>
2019-02-11 13:44:53 -0600

    Add getPlayerId() and getPlayerName()

---
+++ R/getPlayerId.R
@@ -0,0 +1,29 @@
+#' Get player ID
+#'
+#' Get the ID for a player with a given name.
+#'
+#' @md
+#'
+#' @param player_name Character string with name of a player.
+#'
+#' @return A number representing the ID of the player.
+#'
+#' @examples
+#' \dontrun{
+#' mc_connect()
+#' getPlayerIds()
+#' getPlayerId("wyldstyle")
+#' }
+#'
+#' @seealso [getPlayerIds()], [getPlayerName()]
+#'
+#' @export
+#'
+
+getPlayerId <- function(player_name)
+{
+
+    result <- mc_sendreceive(merge_data("world.getPlayerId", player_name))
+    as.numeric(result)
+
+}
---
+++ R/getPlayerName.R
@@ -0,0 +1,29 @@
+#' Get player name
+#'
+#' Get the name of a player with a given ID
+#'
+#' @md
+#'
+#' @param player_id Integer giving the ID of a player. You can find IDs of all
+#'   current players using [getPlayerIds()].
+#'
+#' @return A character string with the name of the identified player.
+#'
+#' @examples
+#' \dontrun{
+#' mc_connect()
+#' getPlayerIds()
+#' getPlayerName(322)
+#' }
+#'
+#' @seealso [getPlayerIds()], [getPlayerId()]
+#'
+#' @export
+#'
+
+getPlayerName <- function(player_id)
+{
+
+    mc_sendreceive(merge_data("entity.getName", player_id))
+
+}
